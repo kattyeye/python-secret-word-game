7ca8df0b13747be0b7db717a481d98948e66ab12
Kat Ingram <69052750+kattyeye@users.noreply.github.com>
2021-10-05 11:41:08 -0400

    Adding Initial Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# player gets 7 incorrect guesses
+# don't let player guess same char more than 2 times
+
+# _ _ _ _ _
+# input
+# response
+
+# _ e _ _ _
+
+# loop that keeps firing, if they guess the word kick them out
+
+import random
+
