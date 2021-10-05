45e94e1fab75e98ea3fd067ff40cdf2295a94609
Kat Ingram <69052750+kattyeye@users.noreply.github.com>
2021-10-05 12:35:00 -0400

    Adding game without not allowing player to guess same char twice

--- main.py
+++ main.py
@@ -11,3 +11,42 @@
 
 import random
 
+words_list = ['cat', 'rat']
+
+word = random.choice(words_list)
+
+print("Guess a letter: ")
+
+guesses = ''
+
+
+turns = 7
+
+while turns > 0:
+    failed_attempts = 0
+
+    for char in word:
+        if char in guesses:
+            print(char)
+        else:
+            print('_')
+
+            failed_attempts += 1
+
+    if failed_attempts == 0:
+        print("You win!")
+        print("The word is: ", word)
+        break
+
+    guess = input("Guess a letter: ")
+
+    guesses += guess
+
+    if guess not in word:
+        turns -= 1
+
+        print("Wrong,")
+        print("you have", + turns, "more guesses.")
+
+        if turns == 0:
+            print("You lose")
